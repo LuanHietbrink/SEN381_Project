330c0762a0930a7dfdc7af78b9139fd8cbbbe47a
Leander-Botha <578404@student.belgiumcampus.ac.za>
2023-10-30 12:29:22 +0200

    Filtering Jobs Functionality
    Filter By Job name, date and status

---
+++ Premier Service Solutions/ClientApp/src/components/DateFilter.js
@@ -0,0 +1,29 @@
+// DateFilter.js
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function DateFilter({ selectedStartDate, selectedEndDate, onStartDateChange, onEndDateChange }) {
+    return (
+        <div>
+            <DatePicker
+                selected={selectedStartDate}
+                onChange={onStartDateChange}
+                selectsStart
+                startDate={selectedStartDate}
+                endDate={selectedEndDate}
+                placeholderText="Start Date"
+            />
+            <DatePicker
+                selected={selectedEndDate}
+                onChange={onEndDateChange}
+                selectsEnd
+                startDate={selectedStartDate}
+                endDate={selectedEndDate}
+                placeholderText="End Date"
+            />
+        </div>
+    );
+}
+
+export default DateFilter;
--- Premier Service Solutions/ClientApp/src/components/Departments/ServiceDeptJobList.js
+++ Premier Service Solutions/ClientApp/src/components/Departments/ServiceDeptJobList.js
@@ -1,98 +1,191 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Dept Styles/Service.css";
+import DateFilter from "../DateFilter";
 
 export class ServiceDeptJobList extends Component {
-    static displayName = ServiceDeptJobList.name;
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            user: {
-                username: 'John Doe',
-                profilePicture: '/profile.jpg'
-            },
-            jobs: [
-                {
-                    name: 'Job Name 1',
-                    number: '001',
-                    description: 'Todays VS Code tip: Emmet lorem Just type lorem in #html to generate a paragraph of dummy text.Control how much text is generated with a number suffix, such as lorem10 to generate 10 words of dummy text You can also combine lorem with other Emmet abbreviations.',
-                },
-                {
-                    name: 'Job Name 2',
-                    number: '002',
-                    description: 'Todays VS Code tip: Emmet lorem Just type lorem in #html to generate a paragraph of dummy text.Control how much text is generated with a number suffix, such as lorem10 to generate 10 words of dummy text You can also combine lorem with other Emmet abbreviations.',
-                },
-                {
-                    name: 'Job Name 3',
-                    number: '003',
-                    description: 'Todays VS Code tip: Emmet lorem Just type lorem in #html to generate a paragraph of dummy text.Control how much text is generated with a number suffix, such as lorem10 to generate 10 words of dummy text You can also combine lorem with other Emmet abbreviations.',
-                },
-                // Add more jobs as needed
-            ],
-            selectedJob: null,
-            isModalOpen: false, // Add modal state
-        }
-    }
+    this.state = {
+      user: {
+        username: "John Doe",
+        profilePicture: "/profile.jpg",
+      },
+      serviceRequests: [], // Service requests data
+      selectedJob: null,
+      isModalOpen: false,
+      totalOpenRequests: 0,
+      totalAssignedJobs: 0,
+      techniciansAvailable: 0,
+      filterName: "",
+      selectedStartDate: null,
+      selectedEndDate: null,
+      selectedStatus: "In Progress",
+    };
+  }
 
-    handleViewDetails = (job) => {
-        this.setState({ selectedJob: job, isModalOpen: true });
-    }
+  componentDidMount() {
+    fetch("/api/service-requests")
+      .then((response) => response.json())
+      .then((data) => {
+        this.originalServiceRequests = data;
+        this.setState({ serviceRequests: data });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
 
-    closeModal = () => {
-        this.setState({ isModalOpen: false });
-    }
+  handleViewDetails = (job) => {
+    this.setState({ selectedJob: job, isModalOpen: true });
+  };
 
-    render() {
-        const { selectedJob, isModalOpen } = this.state;
-        return (
-            <div>
-                <h1 className="center-tech">Premier Service Solutions</h1>
-                <div className="user-info">
-                    <span>Hello, {this.state.user.username}</span>
-                    <img
-                        src={this.state.user.profilePicture}
-                        alt="Profile"
-                        className="profile-picture"
-                    />
-                </div>
-                <div className="left-align">
-                    <h2>Service Department</h2>
-                    <div className="line"></div>
-                    <p className="assign">List of Jobs</p>
-                </div>
-                <div className="ticket-list">
-                    {this.state.jobs.map((job, index) => (
-                        <div key={index} className="ticket">
-                            <h5>#{job.number} | {job.name}</h5>
-                            <div className="ticket-content">
-                                <p>{job.description}</p>
-                                <button className="view-details" onClick={() => this.handleViewDetails(job)}>Assign Technician</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                {isModalOpen && (
-                    <Modal
-                        job={selectedJob}
-                        onClose={this.closeModal}
-                    />
-                )}
+  handleNameFilterChange = (e) => {
+    this.setState({ filterName: e.target.value });
+  };
+
+handleStartDateChange = (date) => {
+    this.setState({ selectedStartDate: date }, () => {
+        this.filterServiceRequestsByDate();
+    });
+};
+
+handleEndDateChange = (date) => {
+    this.setState({ selectedEndDate: date }, () => {
+        this.filterServiceRequestsByDate();
+    });
+};
+
+handleStatusFilterChange = (e) => {
+    this.setState({ selectedStatus: e.target.value });
+};
+
+
+
+filterServiceRequestsByDate = () => {
+const extractDatePart = (date) => new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+  const { selectedStartDate, selectedEndDate } = this.state;
+  const { originalServiceRequests } = this;
+
+  if (selectedStartDate && selectedEndDate) {
+    const filteredRequests = originalServiceRequests.filter((request) => {
+      const requestDate = new Date(request.requestDate);
+      const requestDateOnly = extractDatePart(requestDate);
+      const selectedStartDateOnly = extractDatePart(selectedStartDate);
+      const selectedEndDateOnly = extractDatePart(selectedEndDate);
+
+      return requestDateOnly >= selectedStartDateOnly && requestDateOnly <= selectedEndDateOnly;
+    });
+    this.setState({ serviceRequests: filteredRequests });
+  } else {
+    this.setState({ serviceRequests: originalServiceRequests });
+  }
+};
+
+  
+  
+
+
+
+  render() {
+    const {
+      serviceRequests,
+      filterName,
+      selectedJob,
+      isModalOpen,
+      selectedEndDate,
+      selectedStartDate,
+      selectedStatus,
+    } = this.state;
+
+
+    const filteredServiceRequests = serviceRequests.filter((request) =>
+    (selectedStatus === "" || request.status === selectedStatus) &&
+    (filterName === "" || request.requestDetails.toLowerCase().includes(filterName.toLowerCase()))
+    );
+
+
+    return (
+      <div>
+        <h1 className="center-tech">Premier Service Solutions</h1>
+        <div className="user-info">
+          <span>Hello, {this.state.user.username}</span>
+          <img
+            src={this.state.user.profilePicture}
+            alt="Profile"
+            className="profile-picture"
+          />
+        </div>
+        <div className="left-align">
+          <h2>Service Department</h2>
+          <div className="line"></div>
+          <p className="assign">List of Jobs</p>
+        </div>
+        <div className="filter-container">
+          <input
+            type="text"
+            placeholder="Search"
+            value={filterName}
+            onChange={this.handleNameFilterChange}
+          />
+        </div>
+        <div className="filter-container">
+          <DateFilter
+            selectedStartDate={selectedStartDate}
+            selectedEndDate={selectedEndDate}
+            onStartDateChange={this.handleStartDateChange}
+            onEndDateChange={this.handleEndDateChange}
+          />
+
+        </div>
+        <div className="filter-container">
+          <select
+            value={selectedStatus}
+            onChange={this.handleStatusFilterChange}
+          >
+            <option value="">All Statuses</option>
+            <option value="In Progress">In-Progress</option>
+            <option value="Completed">Completed</option>
+            <option value="Delayed">Delayed</option>
+          </select>
+        </div>
+
+        <div className="ticket-list">
+          {filteredServiceRequests.map((request) => (
+            <div key={request.requestId} className="ticket">
+              <h5>
+                #{request.requestId} | {request.requestDetails}
+              </h5>
+              <div className="ticket-content">
+                <p>
+                  {request.status}, request created on {request.requestDate}
+                </p>
+                <button
+                  className="view-details"
+                  onClick={() => this.handleViewDetails(request)}
+                >
+                  View Details
+                </button>
+              </div>
             </div>
-        );
-    }
+          ))}
+        </div>
+        {isModalOpen && <Modal job={selectedJob} onClose={this.closeModal} />}
+      </div>
+    );
+  }
 }
 
 class Modal extends Component {
-    render() {
-        const { job, onClose } = this.props;
-        return (
-            <div className="modal">
-                <div className="modal-content">
-                    <h3>{job.name}</h3>
-                    <p>{job.description}</p>
-                    <button onClick={onClose}>Close</button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { job, onClose } = this.props;
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <h3>{job.requestDetails}</h3>
+          <p>{job.status}</p>
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>
+    );
+  }
 }
